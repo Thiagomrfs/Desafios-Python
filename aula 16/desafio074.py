84563c2fda05ce470edcad6e7dcdcf8f3e6053a9
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-04 21:28:35 -0300

    tupla com números aleatórios

--- aula 16/desafio074.py
+++ aula 16/desafio074.py
@@ -1,1 +1,8 @@
 #gere 5 números aleatórios e coloque numa tupla, depois mostre os valores e qual o maior e menor
+from random import randint
+
+num = (randint(1, 100), randint(1, 100), randint(1, 100), randint(1, 100), randint(1, 100))
+maior = max(num)
+menor = min(num)
+print(num)
+print(f"{maior} é o maior valor e {menor} é o menor valor.")
