2c313505d759fb05ee69872fe5749581ccf6344a
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-04 21:19:04 -0300

    Percorrendo tuplas

--- aula 16/desafio073.py
+++ aula 16/desafio073.py
@@ -1,2 +1,18 @@
 # pegue uma tabela com os 20 colocados do campeonato brasileiro de fut e depois mostre:
 # 5 primeiros, 5 ultimos, times em ordem alfabética, where is chapecoense
+
+bras = ("Flamengo", "Santos", "Palmeiras", "Grêmio", "Atlético - PR", "São Paulo", "Internacional", "Corinthians",
+        "Fortaleza", "Goiás", "Bahia", "Vasco", "Atlético - MG", "Fluminense", "Botafogo", "Ceará", "Cruzeiro",
+        "CSA", "Chapecoense", "Avaí")
+
+first5 = bras[:5]
+last5 = bras[16:]
+OA = sorted(bras)
+chape = bras.index("Chapecoense")
+
+print(f"""
+os 5 primeiros são: {first5}
+os 5 ultimos são: {last5}
+em ordem alfabética: {OA}
+chapecoense está na {chape+1} colocação
+""")
