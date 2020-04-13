27e4ea2480a8e0b4c266983d829747dd12a80f9a
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-13 19:07:16 -0300

    ordenando números sem usar a função sort()

--- aula 17/desafio080.py
+++ aula 17/desafio080.py
@@ -7,9 +7,17 @@ for c in range(0, 5):
     if c == 0:
         list.append(num)
     else:
-        if num < min(list):
-            list.insert(list.index(min(list)), num)
-        if num > max(list):
-            list.insert(list.index(min(list))+1, num)
+        if num >= max(list):
+            list.append(num)
+        elif num <= min(list):
+            list.insert(0, num)
+        elif min(list) < num < max(list):
+            if num < list[1]:
+                list.insert(1, num)
+            elif list[1] < num < list[2]:
+                list.insert(2, num)
+            elif list[2] < num < list[3]:
+                list.insert(3, num)
+
 
 print(list)
--- aula 17/desafio083.py
+++ aula 17/desafio083.py
@@ -1,2 +1,3 @@
 # leia uma expressão qualquer que use parênteses, o programa deve dizer se os parenteses estão arbetos e fechados na
 # ordem certa
+
