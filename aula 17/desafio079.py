5306f563be4ae1e5908e714b390f9e5a705cb055
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-13 17:06:01 -0300

    utilizando .append() e evitando repetições

--- aula 17/desafio079.py
+++ aula 17/desafio079.py
@@ -1,2 +1,27 @@
 # leia vários valores numéricos e coloque em lista, se já tiver o número lá dentro ele não adiciona(só adc novos
 # valores), depois mostre os valores em ordem crescente
+list = []
+
+
+def ver(x, y):
+    if x not in y:
+        while x not in y:
+            x = input("\033[31mCOMANDO INVÁLIDO, POR FAVOR INSIRA UM VALOR VÁLIDO[S/N]: \033[m").upper()
+    return x
+
+
+while True:
+    num = int(input("Digite o seu número: "))
+    if num in list:
+        print("O número já consta na lista.")
+    else:
+        print("Número adicionado com sucesso.")
+        list.append(num)
+    resp = input("Deseja continuar[S/N]? ").upper()
+    resp = ver(resp, "SN")
+    if resp == "N":
+        break
+
+print(f"""Seus números são:
+{str(sorted(list)).strip("[]")}
+""")
