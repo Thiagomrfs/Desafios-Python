b4d3a63fe7fb7bed427771cee0e2d14a8b4b9d93
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-13 18:23:05 -0300

    ordenando listas com sorted(list, reverse=bool)

--- aula 17/desafio080.py
+++ aula 17/desafio080.py
@@ -1,1 +1,15 @@
 # leia 5 valores numéricos e coloque na posição correta sem usar o sort()
+list = []
+menor = 0
+
+for c in range(0, 5):
+    num = int(input("insira um número: "))
+    if c == 0:
+        list.append(num)
+    else:
+        if num < min(list):
+            list.insert(list.index(min(list)), num)
+        if num > max(list):
+            list.insert(list.index(min(list))+1, num)
+
+print(list)
--- aula 17/desafio081.py
+++ aula 17/desafio081.py
@@ -1,1 +1,34 @@
 # leia vários números e diga a quantidade de valores, lista decrescente e se o 5 estpa na lista
+
+list = []
+count = 0
+
+
+def ver(x, y):
+    if x not in y:
+        while x not in y:
+            x = input("\033[31mCOMANDO INVÁLIDO, POR FAVOR INSIRA UM VALOR VÁLIDO[S/N]: \033[m").upper()
+    return x
+
+
+while True:
+    num = int(input(f"insira o {count+1}º número: "))
+    list.append(num)
+    sorted(list)
+    count += 1
+    resp = input("deseja continuar[S/N]? ").upper()
+    resp = ver(resp, "SN")
+    if resp == "N":
+        break
+
+has5 = ""
+if 5 in list:
+    has5 = "tem"
+else:
+    has5 = "não tem"
+
+print(f"""
+você digitou {count} valores.
+a sua lista decrescente ficou: {sorted(list, reverse=True)}
+na sua lista {has5} o número 5
+""")
