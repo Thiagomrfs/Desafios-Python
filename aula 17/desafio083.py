7369f9e4d3668e846b7a52fd85b3c75c21a481d7
Thiagomrfs <thiagomarbsn@gmail.com>
2020-04-14 19:06:33 -0300

    Validando expressões matemáticas por meio de listas

--- aula 17/desafio083.py
+++ aula 17/desafio083.py
@@ -1,3 +1,37 @@
 # leia uma expressão qualquer que use parênteses, o programa deve dizer se os parenteses estão arbetos e fechados na
 # ordem certa
 
+
+def ver(x, y):
+    if x not in y:
+        while x not in y:
+            x = input("\033[31mCOMANDO INVÁLIDO, POR FAVOR INSIRA UM VALOR VÁLIDO[S/N]: \033[m").upper()
+    return x
+
+
+quant = []
+
+while True:
+    exp = input("Insira a sua expressão: ")
+    for letter in exp:
+        if letter == "(":
+            quant.append("(")
+        else:
+            if len(quant) != 0:
+                quant.pop()
+                print("tirei um parentese")
+            elif len(quant) == 0:
+                quant.append(")")
+                break
+                print("TAVA ZERADO, ADICIONEI UM )")
+    print(len(quant))
+    if len(quant) == 0:
+        print("\033[34mSua expressão é válida!\033[m")
+    else:
+        print("\033[31mSua expressão NÃO é válida!\033[m")
+    resp = input("deseja continuar[S/N]? ").upper()
+    resp = ver(resp, "SN")
+    if resp == "N":
+        break
+    elif resp == "S":
+        quant = []
